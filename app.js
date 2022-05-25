6abf575ca70e681d757f4bd32ac6ceb678d64537
tamahhamat <98357200+tamahhamat@users.noreply.github.com>
2022-05-26 01:13:39 +0200

    added photos
    added photos to grid

---
+++ app.js
@@ -0,0 +1,22 @@
+(() => {
+
+    ham.addEventListener('click', () => {
+        document.getElementById('ham').classList.toggle('open');
+        document.getElementById('nav').classList.toggle('visible');
+        document.getElementById('hone').classList.toggle('hidden');
+        document.getElementById('btn').classList.toggle('hidden');
+    });
+
+    const observerOptions = {
+        rootMargin: '1000px 0px 0px 0px',
+        threshold: 0.5
+    };
+
+    const observe = entries => entries.forEach(entry => {
+        entry.target.classList.toggle('inviewport', entry.isIntersecting);
+    });
+
+    const obs = new IntersectionObserver(observe, observerOptions);
+    document.querySelectorAll('#grid>div').forEach(el => obs.observe(el));
+
+})();
