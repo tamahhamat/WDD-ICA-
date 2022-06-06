b93e4ecc55ee840a30408bee75deaf0a8253c524
tamahhamat <98357200+tamahhamat@users.noreply.github.com>
2022-06-06 16:03:05 -0700

    so many things
    buttons
    grid
    font
    animations
    many mistakes
    many hours

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
         document.getElementById('ham').classList.toggle('open');
         document.getElementById('nav').classList.toggle('visible');
         document.getElementById('hone').classList.toggle('hidden');
-        document.getElementById('btn').classList.toggle('hidden');
+        document.getElementById('grid').classList.toggle('hidden');
+        document.getElementById('links').classList.toggle('hidden');
     });
 
     const observerOptions = {
